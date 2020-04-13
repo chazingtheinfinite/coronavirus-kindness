91bd9dd913fd0dbb5e0ec5710cba78dab6756ec2
Kevin Dick <kevin.dick9@gmail.com>
2020-04-13 12:34:37 -0400

    Fixing layout

--- map.js
+++ map.js
@@ -8,7 +8,7 @@ var heartMarker = L.icon({
 });
 
 // Initialize the basemap to the container div
-var mymap = L.map('mapid').setView([0, 0], 1);
+var mymap = L.map('mapid').setView([0, 0], 2);
 
 // Set the basemap
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
