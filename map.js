509b4f194d5c52f1fe87aff4cc4a679bdeb26445
Kevin Dick <kevin.dick9@gmail.com>
2020-04-13 11:04:38 -0400

    Added css

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 
-var mymap = L.map('mapid').setView([0, 0], 0);
+var mymap = L.map('mapid').setView([0, 0], 1);
 
 /*L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
              maxZoom: 18,
