7fe48b899f166936ee0ae70392a14c1ee8772e81
Kevin Dick <kevin.dick9@gmail.com>
2020-04-18 23:35:02 -0400

    Added dynamic word wrap

--- map.js
+++ map.js
@@ -43,7 +43,7 @@ d3.csv(publishedData, function(d) {
 					contributeHtml = "<br><br>Want to help? &nbsp; <a href='" + data[i].contribute + "' target='_blank'><button class='button-small'><b>Contribute</b></button></a>";
 					
 					// Append this article to the contribution DIV
-					document.getElementById('contribute').innerHTML += "<tr><td><a href='" + data[i].contribute + "' target='_blank'><button class='button'><b>Contribute</b></button></a></td><td word-wrap='break-word'><b><a href='" + data[i].url + "' target='_blank'>" + data[i].title + "</a></b></td></tr><br>";
+					document.getElementById('contribute').innerHTML += "<tr><td><a href='" + data[i].contribute + "' target='_blank'><button class='button'><b>Contribute</b></button></a></td><td id='towrap' word-wrap='break-word'><b><a href='" + data[i].url + "' target='_blank'>" + data[i].title + "</a></b></td></tr><br>";
 				}
 				// Create the Marker!
 				L.marker([data[i].lat, data[i].lon], {icon: heartMarker}).addTo(mymap).bindPopup("<b><a href='" + data[i].url + "' target='_blank'>" + data[i].title + "</a></b><br>Date: " + data[i].date + "<br>" + data[i].giver + " &#8594; " + data[i].recipient + "<br>"+ data[i].info + contributeHtml).openPopup();
@@ -51,6 +51,9 @@ d3.csv(publishedData, function(d) {
 			
 			// After parsing all entries, we need to close the table in the contribution section!
 			document.getElementById('contribute').innerHTML += "</table>";
+
+			// Wrap wording in all table descriptions
+			document.getElementById("towrap").style.wordWrap = "break-word";
 		});
 
 
